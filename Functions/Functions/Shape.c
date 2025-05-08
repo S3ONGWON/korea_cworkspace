db1002a6d891750f57c40ad588832f64c56db53b
S3ONGWON <seongwon1121@gmail.com>
2025-05-08 21:54:59 +0900

    function

---
+++ Functions/Functions/Shape.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int square(int x)
+{
+	return x * x;
+}
+float triangle(int size, int height)
+{
+	//강제형변환 해야 실수로 반한됨
+	float area = (float)(size * height) / 2;
+	return area;
+}
+
+int main()
+{
+	int rectArea;
+	float triArea;
+
+	//정사각형의 넓이 계산
+	rectArea = square(4);
+
+	//삼각형의 넓이 계산
+	triArea = triangle(3, 5);
+
+	printf("정사각형의 넓이 : %dcm\n", rectArea);
+	printf("삼각형의 넓이 : %.1fcm\n", triArea);
+
+	return 0;
+}
